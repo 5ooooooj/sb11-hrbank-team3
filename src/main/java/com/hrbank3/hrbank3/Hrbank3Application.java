8d4fe469f92004a4b13ae08d4fca277e612fb85e
Yunsun <justina7182@gmail.com>
2026-04-13 16:00:29 +0900

    chore: initial project setup add .gitignore and project structure, set up Spring Boot with Gradle dependencies, configure applicaiton.yml

---
+++ src/main/java/com/hrbank3/hrbank3/Hrbank3Application.java
@@ -0,0 +1,13 @@
+package com.hrbank3.hrbank3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hrbank3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hrbank3Application.class, args);
+	}
+
+}

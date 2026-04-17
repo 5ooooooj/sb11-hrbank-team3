ca27fdd7378d9c4585c76ccaf40c61bb7a950f20
Yunsun <justina7182@gmail.com>
2026-04-17 14:07:59 +0900

    Fix: BackupHistoryMapper 빌드 문제 해결 [#43]

--- src/main/java/com/hrbank3/hrbank3/mapper/BackupHistoryMapper.java
+++ src/main/java/com/hrbank3/hrbank3/mapper/BackupHistoryMapper.java
@@ -9,17 +9,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface BackupHistoryMapper extends GenericMapper<BackupHistoryDto, BackupHistory> {
+public interface BackupHistoryMapper  {
 
   // 서울 기준시간 상수로 정의
   ZoneId KST = ZoneId.of("Asia/Seoul");
 
   @Mapping(target = "fileId", source = "file.id") // 객체에서 아이디만 꺼내기
   BackupHistoryDto toDto(BackupHistory entity);
-
-  @Mapping(target = "file", ignore = true) // fileId로 file 객체를 만들 수 없으므로 MapStruct가 무시하게 하고 Service에서 처리 후 넘겨줌
-  BackupHistory toEntity(BackupHistoryDto dto);
-
 
   // MapStruct는 매핑시 타입이 맞지 않으면 같은 Mapper 안에 변환 메서드가 있는지 찾고 자동으로 끼워넣음 그걸 위해 메소드 정의
   default ZonedDateTime toZonedDateTime(Instant instant) {

2e75247ae9824a483a8cacad1ff831a42ad6bc8a
강우진 <spacein1001@gmail.com>
2026-04-17 11:42:02 +0900

    Feat: 직원 목록 조회 Service 및 Repository 구현 [#30]

--- src/main/java/com/hrbank3/hrbank3/repository/EmployeeRepository.java
+++ src/main/java/com/hrbank3/hrbank3/repository/EmployeeRepository.java
@@ -1,10 +1,15 @@
 package com.hrbank3.hrbank3.repository;
 
 import com.hrbank3.hrbank3.entity.Employee;
+import com.hrbank3.hrbank3.repository.custom.EmployeeRepositoryCustom;
+import java.time.Instant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long>,
+    EmployeeRepositoryCustom {
 
   // 기본적인 CRUD는 JpaRepository가 기본 제공함
   boolean existsByEmail(String email);
+
+  boolean existsByUpdatedAtAfter(Instant lastBackupTime);
 }
---
+++ src/main/java/com/hrbank3/hrbank3/repository/custom/EmployeeRepositoryCustom.java
@@ -0,0 +1,14 @@
+package com.hrbank3.hrbank3.repository.custom;
+
+import com.hrbank3.hrbank3.entity.Employee;
+import com.hrbank3.hrbank3.repository.condition.EmployeeSearchCondition;
+import java.util.List;
+
+public interface EmployeeRepositoryCustom {
+
+  List<Employee> findAllByCondition(EmployeeSearchCondition condition);
+  // 조건에 맞는 직원 목록 조회시 사용
+
+  long countByCondition(EmployeeSearchCondition condition);
+  // 전체 직원 수 조회에 사용
+}

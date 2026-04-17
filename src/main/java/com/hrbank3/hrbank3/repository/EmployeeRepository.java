d0e730bbb936790558bd48969b2804bf81366cb1
habin08090 <gkqls0809@gmail.com>
2026-04-17 16:01:02 +0900

    Feat: 대시보드 카드형 요약 정보 API 구현 [#41]

--- src/main/java/com/hrbank3/hrbank3/repository/EmployeeRepository.java
+++ src/main/java/com/hrbank3/hrbank3/repository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.hrbank3.hrbank3.repository;
 
 import com.hrbank3.hrbank3.entity.Employee;
+import com.hrbank3.hrbank3.entity.EmployeeStatus;
 import java.time.Instant;
+import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
@@ -11,4 +13,10 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
   // 특정 시간 이후 변경된 직원 존재 여부 - BackupHistoryService에서 사용함
   boolean existsByUpdatedAtAfter(Instant lastBackupTime);
+
+  // 퇴사자 제외한 총 직원 수 - 대시보드에서 사용
+  long countByStatusNot(EmployeeStatus status);
+
+  // 이번달 입사자 수 - 대시보드에서 사용
+  long countByHireDateGreaterThanEqualAndStatus(LocalDate hireDate, EmployeeStatus status);
 }

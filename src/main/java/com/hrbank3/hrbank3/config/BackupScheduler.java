48715c5c054bae9147db8f79883b0f07114641ce
Yunsun <justina7182@gmail.com>
2026-04-20 16:47:03 +0900

    Feat: 백업 프로세스 배치 스케줄러 구현 [#69] - @Scheduled로 백업서비스 호출 - 개발환경에 따른 배치 주기 설정

---
+++ src/main/java/com/hrbank3/hrbank3/config/BackupScheduler.java
@@ -0,0 +1,26 @@
+package com.hrbank3.hrbank3.config;
+
+import com.hrbank3.hrbank3.service.BackupHistoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BackupScheduler {
+
+  private final BackupHistoryService backupHistoryService;
+
+  @Scheduled(cron = "${backup.schedule.cron}")
+  public void runBackup() {
+    log.info("[BackupScheduler] 배치 작업 시작");
+    try {
+      backupHistoryService.backup("system");
+    } catch (Exception e) {
+      log.error("[BackupScheduler] 배치 작업 중 예외 발생: ", e);
+    }
+  }
+
+}
